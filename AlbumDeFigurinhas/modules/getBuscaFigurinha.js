3c048f2d414ecd38cdf1b4b2956228c0c6f6377b
Paraizo <eduardoparize.05@gmail.com>
2025-10-13 11:51:34 -0300

    finalizado a funcao de busca

--- AlbumDeFigurinhas/modules/getBuscaFigurinha.js
+++ AlbumDeFigurinhas/modules/getBuscaFigurinha.js
@@ -1,20 +1,22 @@
 import { figurinha } from "../schema/FIgurinhasSchema.js";
 
 export async function getBuscaFigurinha(req, res) {
-  const { numero, tema } = req.query;
+  const { nome, numero, tema } = req.query;
   const query = {};
-
+  if (nome) {
+    query.nome =  { $regex: new RegExp(nome, 'i') };
+  }
   if (numero) {
-    query = query.numero;
+    query.numero = numero;
   }
   if (tema) {
-    query = query.tema;
+    query.tema = { $regex: new RegExp(tema, 'i') };;
   }
   try {
     const figurinhasFiltrada = await figurinha.find(query);
     if (!figurinhasFiltrada || figurinhasFiltrada.length == 0) {
       return res
-        .status(400)
+        .status(404)
         .send("Nenhuma figurinha encontrada com esses parametros.");
     }
     return res.status(200).send(figurinhasFiltrada);
